c1bb16858a10774b04fb6d37e236a68323afb2f7
parkks.free <parkks.free@gmail.com>
2023-02-05 20:00:08 +0900

    Initial Commit

---
+++ BitriseSample/BitriseSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitriseSampleApp.swift
+//  BitriseSample
+//
+//  Created by パク・ギョンソク on 2023/02/05.
+//
+
+import SwiftUI
+
+@main
+struct BitriseSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
