fc68281d0fff6a70d293e2b892d0e80bdc3d45c8
parkks.free <parkks.free@gmail.com>
2023-02-05 20:19:59 +0900

    First commit

---
+++ BitriseSampleTests/BitriseSampleTests.swift
@@ -0,0 +1,26 @@
+//
+//  BitriseSampleTests.swift
+//  BitriseSampleTests
+//
+//  Created by パク・ギョンソク on 2023/02/05.
+//
+
+import XCTest
+
+final class BitriseSampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testAdd() {
+        var age = 20
+        age += 1
+        
+        XCTAssertEqual(age, 21)
+    }
+}
